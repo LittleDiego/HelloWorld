dfbf5ed19280becb31301aeaf954ecbd0e45db06
PeteyPab <ppresedo@protonmail.com>
2021-10-06 19:11:51 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+course = "Python's course for Beginners"
+# print("Pablo")
+#
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+#
+# price = 10
+# full_name = 'John Smith'
+# age = 20
+# is_new: bool = True
+
+# if is_new:
+#     print(full_name)
+#
+# name = input('What is your name? ')
+# favColor = input('What is your favorite color? ')
+# print('Hi ' + name + ' I like ' + favColor + ' too.')
+
+# weightinlbs = input('Weight (lbs): ')
+# weightinKg = float(weightinlbs) / 2.2
+# print(weightinKg)
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(age))
+# print(age)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
